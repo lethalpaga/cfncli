afbe4850e58688225c264f3807654964d012f3c8
lethalpaga <lethalpaga@gmail.com>
2016-02-02 19:35:27 +1300

    Add unit tests for all state functions

--- spec/lib/cfncli/stack_spec.rb
+++ spec/lib/cfncli/stack_spec.rb
@@ -15,95 +15,185 @@ describe CfnCli::Stack do
     stack
   end
 
-  let(:client) {stack.cfn.client}
+  describe 'Stack state detection' do
+    let(:client) {stack.cfn.client}
 
-  let(:updated_stack) { describe_stacks_resp('UPDATE_COMPLETE') }
-  let(:update_failed_stack) { describe_stacks_resp('UPDATE_FAILED') }
-  let(:update_in_progress_stack) { describe_stacks_resp('UPDATE_IN_PROGRESS') }
+    let(:updated_stack) { describe_stacks_resp('UPDATE_COMPLETE') }
+    let(:update_failed_stack) { describe_stacks_resp('UPDATE_FAILED') }
+    let(:update_in_progress_stack) { describe_stacks_resp('UPDATE_IN_PROGRESS') }
 
-  before do
-    client.stub_responses :describe_stacks, stubbed_response
+    before do
+      client.stub_responses :describe_stacks, stubbed_response
+    end
+
+    describe '#finished?' do
+      subject { stack.finished? }
+
+      context 'when in a finished state' do
+        let(:stubbed_response) { updated_stack }
+
+        it { is_expected.to be true }
+      end
+
+      context 'when in a transition state' do
+        let(:stubbed_response) { update_in_progress_stack }
+
+        it { is_expected.to be false }
+      end
+    end
+
+    describe '#succeeded' do
+      subject { stack.succeeded? }
+
+      context 'when in a successful state' do
+        let(:stubbed_response) { updated_stack }
+
+        it { is_expected.to be true }
+      end
+      
+      context 'when in a transition state' do
+        let(:stubbed_response) { update_in_progress_stack }
+
+        it { is_expected.to be false }
+      end
+      
+      context 'when in a failed state' do
+        let(:stubbed_response) { update_failed_stack }
+
+        it { is_expected.to be false }
+      end
+    end
+
+    describe '#in_progress' do
+      subject { stack.in_progress? }
+
+      context 'when in a successful state' do
+        let(:stubbed_response) { updated_stack }
+
+        it { is_expected.to be false }
+      end
+      
+      context 'when in a transition state' do
+        let(:stubbed_response) { update_in_progress_stack }
+
+        it { is_expected.to be true }
+      end
+      
+      context 'when in a failed state' do
+        let(:stubbed_response) { update_failed_stack }
+
+        it { is_expected.to be false }
+      end
+    end
+
+    describe '#failed' do
+      subject { stack.failed? }
+
+      context 'when in a successful state' do
+        let(:stubbed_response) { updated_stack }
+
+        it { is_expected.to be false }
+      end
+      
+      context 'when in a transition state' do
+        let(:stubbed_response) { update_in_progress_stack }
+
+        it { is_expected.to be false }
+      end
+      
+      context 'when in a failed state' do
+        let(:stubbed_response) { update_failed_stack }
+
+        it { is_expected.to be true }
+      end
+    end
   end
 
-  describe '#finished?' do
-    subject { stack.finished? }
+  describe '#transitive_states' do
+     subject { stack.transitive_states }
 
-    context 'when in a finished state' do
-      let(:stubbed_response) { updated_stack }
+     it { is_expected.to include 'CREATE_IN_PROGRESS' }
+     it { is_expected.to include 'ROLLBACK_IN_PROGRESS' }
+     it { is_expected.to include 'DELETE_IN_PROGRESS' }
+     it { is_expected.to include 'CREATE_IN_PROGRESS' }
+     it { is_expected.to include 'UPDATE_COMPLETE_CLEANUP_IN_PROGRESS' }
+     it { is_expected.to include 'UPDATE_IN_PROGRESS' }
+     it { is_expected.to include 'UPDATE_ROLLBACK_IN_PROGRESS' }
+     it { is_expected.to include 'UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS' }
 
-      it { is_expected.to be true }
-    end
-
-    context 'when in a transition state' do
-      let(:stubbed_response) { update_in_progress_stack }
-
-      it { is_expected.to be false }
-    end
+     it { is_expected.not_to include 'CREATE_FAILED' }
+     it { is_expected.not_to include 'CREATE_COMPLETE' }
+     it { is_expected.not_to include 'ROLLBACK_FAILED' }
+     it { is_expected.not_to include 'ROLLBACK_COMPLETE' }
+     it { is_expected.not_to include 'UPDATE_COMPLETE' }
+     it { is_expected.not_to include 'UPDATE_ROLLBACK_FAILED' }
+     it { is_expected.not_to include 'UPDATE_ROLLBACK_COMPLETE' }
+     it { is_expected.not_to include 'DELETE_COMPLETE' }
   end
 
-  describe '#succeeded' do
-    subject { stack.succeeded? }
+  describe '#finished_states' do
+     subject { stack.finished_states }
 
-    context 'when in a successful state' do
-      let(:stubbed_response) { updated_stack }
+     it { is_expected.to include 'CREATE_COMPLETE' }
+     it { is_expected.to include 'UPDATE_COMPLETE' }
+     it { is_expected.to include 'DELETE_COMPLETE' }
+     it { is_expected.to include 'CREATE_FAILED' }
+     it { is_expected.to include 'ROLLBACK_FAILED' }
+     it { is_expected.to include 'ROLLBACK_COMPLETE' }
+     it { is_expected.to include 'UPDATE_ROLLBACK_FAILED' }
+     it { is_expected.to include 'UPDATE_ROLLBACK_COMPLETE' }
 
-      it { is_expected.to be true }
-    end
-    
-    context 'when in a transition state' do
-      let(:stubbed_response) { update_in_progress_stack }
-
-      it { is_expected.to be false }
-    end
-    
-    context 'when in a failed state' do
-      let(:stubbed_response) { update_failed_stack }
-
-      it { is_expected.to be false }
-    end
+     it { is_expected.not_to include 'CREATE_IN_PROGRESS' }
+     it { is_expected.not_to include 'ROLLBACK_IN_PROGRESS' }
+     it { is_expected.not_to include 'DELETE_IN_PROGRESS' }
+     it { is_expected.not_to include 'CREATE_IN_PROGRESS' }
+     it { is_expected.not_to include 'UPDATE_COMPLETE_CLEANUP_IN_PROGRESS' }
+     it { is_expected.not_to include 'UPDATE_IN_PROGRESS' }
+     it { is_expected.not_to include 'UPDATE_ROLLBACK_IN_PROGRESS' }
+     it { is_expected.not_to include 'UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS' }
   end
 
-  describe '#in_progress' do
-    subject { stack.in_progress? }
+  describe '#success_states' do
+     subject { stack.success_states }
 
-    context 'when in a successful state' do
-      let(:stubbed_response) { updated_stack }
+     it { is_expected.to include 'CREATE_COMPLETE' }
+     it { is_expected.to include 'UPDATE_COMPLETE' }
+     it { is_expected.to include 'DELETE_COMPLETE' }
 
-      it { is_expected.to be false }
-    end
-    
-    context 'when in a transition state' do
-      let(:stubbed_response) { update_in_progress_stack }
-
-      it { is_expected.to be true }
-    end
-    
-    context 'when in a failed state' do
-      let(:stubbed_response) { update_failed_stack }
-
-      it { is_expected.to be false }
-    end
+     it { is_expected.not_to include 'CREATE_IN_PROGRESS' }
+     it { is_expected.not_to include 'ROLLBACK_IN_PROGRESS' }
+     it { is_expected.not_to include 'DELETE_IN_PROGRESS' }
+     it { is_expected.not_to include 'CREATE_IN_PROGRESS' }
+     it { is_expected.not_to include 'UPDATE_COMPLETE_CLEANUP_IN_PROGRESS' }
+     it { is_expected.not_to include 'UPDATE_IN_PROGRESS' }
+     it { is_expected.not_to include 'UPDATE_ROLLBACK_IN_PROGRESS' }
+     it { is_expected.not_to include 'UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS' }
+     it { is_expected.not_to include 'CREATE_FAILED' }
+     it { is_expected.not_to include 'ROLLBACK_FAILED' }
+     it { is_expected.not_to include 'ROLLBACK_COMPLETE' }
+     it { is_expected.not_to include 'UPDATE_ROLLBACK_FAILED' }
+     it { is_expected.not_to include 'UPDATE_ROLLBACK_COMPLETE' }
   end
 
-  describe '#failed' do
-    subject { stack.failed? }
+  describe '#failed_states' do
+     subject { stack.failed_states }
+     
+     it { is_expected.to include 'ROLLBACK_FAILED' }
+     it { is_expected.to include 'ROLLBACK_COMPLETE' }
+     it { is_expected.to include 'UPDATE_ROLLBACK_FAILED' }
+     it { is_expected.to include 'UPDATE_ROLLBACK_COMPLETE' }
+     it { is_expected.to include 'CREATE_FAILED' }
 
-    context 'when in a successful state' do
-      let(:stubbed_response) { updated_stack }
-
-      it { is_expected.to be false }
-    end
-    
-    context 'when in a transition state' do
-      let(:stubbed_response) { update_in_progress_stack }
-
-      it { is_expected.to be false }
-    end
-    
-    context 'when in a failed state' do
-      let(:stubbed_response) { update_failed_stack }
-
-      it { is_expected.to be true }
-    end
+     it { is_expected.not_to include 'CREATE_COMPLETE' }
+     it { is_expected.not_to include 'UPDATE_COMPLETE' }
+     it { is_expected.not_to include 'DELETE_COMPLETE' }
+     it { is_expected.not_to include 'CREATE_IN_PROGRESS' }
+     it { is_expected.not_to include 'ROLLBACK_IN_PROGRESS' }
+     it { is_expected.not_to include 'DELETE_IN_PROGRESS' }
+     it { is_expected.not_to include 'CREATE_IN_PROGRESS' }
+     it { is_expected.not_to include 'UPDATE_COMPLETE_CLEANUP_IN_PROGRESS' }
+     it { is_expected.not_to include 'UPDATE_IN_PROGRESS' }
+     it { is_expected.not_to include 'UPDATE_ROLLBACK_IN_PROGRESS' }
+     it { is_expected.not_to include 'UPDATE_ROLLBACK_COMPLETE_CLEANUP_IN_PROGRESS' }
   end
 end
