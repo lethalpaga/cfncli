8a8db726cc34d29fd64076d76ef455a0b23d3e54
lethalpaga <lethalpaga@gmail.com>
2016-04-13 00:21:59 +0000

    Fix unit tests

--- spec/lib/cfncli/cloudformation_spec.rb
+++ spec/lib/cfncli/cloudformation_spec.rb
@@ -41,15 +41,18 @@ describe CfnCli::CloudFormation do
     let(:stack) { double CfnCli::Stack }
 
     before do
-      expect(cfn).to receive(:create_stack_obj).and_return(stack)
+      allow(cfn).to receive(:create_stack_obj).and_return(stack)
       allow(stack).to receive(:exists?).and_return(exists)
+      allow(stack).to receive(:stack_id).and_return('stack_id')
+      allow(stack).to receive(:stack_name).and_return('stack_name')
+      allow(stack).to receive(:list_events).and_return('stack_name')
     end
 
     context 'when the stack exists' do
       let(:exists) { true }
       it 'is expected to call stack.delete' do
-        expect(stack).to receive(:delete).with('stack_name', {})
-        expect(stack).to receive(:events).with('stack_name', {})
+        expect(stack).to receive(:delete).with('stack_id', {})
+        expect(stack).to receive(:events).and_return([])
         subject
       end
     end
