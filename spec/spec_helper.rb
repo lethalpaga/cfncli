c7db7653bbad363b4dd44241f644bb3f52b10886
lethalpaga <lethalpaga@gmail.com>
2016-08-30 09:53:25 +1200

    Temporarily disable codeclimate
    It's blocking the release of 0.8 for some reason

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+#require "codeclimate-test-reporter"
+#CodeClimate::TestReporter.start
 
 require 'aws-sdk'
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
